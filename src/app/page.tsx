f47e2d08840dc5752cbfb475c2fc33afc57b399b
an920107 <an920107@gmail.com>
2024-10-03 21:44:15 +0800

    feat: proofreading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function RootPage() {
           dateString="Mar 2023"
           icons={[Icons.cpp]}
         >
-          <p>我在演算法與資料結構領域具亦具有不錯的能力，在當次測驗中排名<b>全國前 6.2%</b>。</p>
+          <p>我在演算法與資料結構領域亦具有不錯的能力，在當次測驗中排名<b>全國前 6.2%</b>。</p>
         </ExperienceParagraph>
       </section>
 
@@ -165,21 +165,21 @@ export default function RootPage() {
         >
           <p>
             <>艾里空是我與朋友創立的公司，專注於開發<b>非接觸式 AIoT</b> 裝置，而我身為公司的資訊長，負責公司的基礎設施、後端 API、前端</>
-            <>應用程式、邊緣裝置部屬以及各項技術的串接與整合。目前已與數家長照機構簽署 MOU 正在進行 PoC，亦正在與桃園市合作進行</>
-            <>的智慧公廁一案。</>
+            <>應用程式、邊緣裝置部署以及各項技術的串接與整合。目前已與數家長照機構簽署 MOU，正在進行 PoC，亦正在與桃園市合作進行</>
+            <>智慧公廁一案。</>
           </p>
         </ExperienceParagraph>
 
         <ExperienceParagraph
           title="LINE TechFRESH 實習生"
-          subtitle="Android Team / Shopping App Project"
+          subtitle="Android Team / Shopping App"
           dateString="Jul 2024 - Jun 2025 (Expected)"
           icons={[Icons.flutter, Icons.kotlin]}
         >
           <p>
-            <>我以實習生的身分參與 LINE 購物 App 的專案開發，為一 <b>DAU 達 10 萬人次</b>的專案。專案使用 Flutter 框架，以 </>
+            <>我以實習生的身分參與 LINE 購物 App 的專案開發，為 <b>DAU 達 10 萬人次</b>的專案。專案使用 Flutter 框架，以 </>
             <><b>Clean Architecture 乾淨架構</b>為基礎，並引入 <b>ATDD 驗收測試驅動開發</b>等較為新興的開發方法。而我在</>
-            <>專案中開發了多項 Native My Account Page 的功能，將改善應用程式該分頁的效能，提升使用者流暢度。</>
+            <>專案中開發了多項 Native My Account Page 的功能，將改善應用程式該分頁的效能，提升使用流暢度。</>
           </p>
         </ExperienceParagraph>
 
@@ -195,7 +195,7 @@ export default function RootPage() {
             <>，協助處室開發一套新的且帶有完整後台功能的</>
             <Link href="https://health.ncu.edu.tw/">網站</Link>
             <>。其中，我負責伺服器硬體組裝、Spec 文件撰寫、UI 設計，以 <b>Next.js 套用 Clean Architecture</b> 開發前端，並善用 </>
-            <>Docker 與 GitHub Workflow 進行<b>自動化集成與部屬</b>，提高開發與測試效率，使的我們能在半年內從零完成網站的開發與建置。</>
+            <>Docker 與 GitHub Workflow 進行<b>自動化集成與部署</b>，提高開發與測試效率，使得我們能在半年內從零完成網站的開發與建置。</>
           </p>
         </ExperienceParagraph>
 
@@ -228,7 +228,7 @@ export default function RootPage() {
         <ExperienceParagraph
           title="課程負責人"
           subtitle="校必修 服務學習、系選修 資訊與社會服務"
-          dateString="Sep 2022 - Jan 2024"
+          dateString="Sep 2022 - Jun 2024"
         >
           <p>
             <>課程培訓資工系同學，強調費曼學習、換位思考，辦理共計 14 場針對國小至高中的程式教育營隊、3 套線上課程影片，並經營網路</>
@@ -259,7 +259,7 @@ export default function RootPage() {
         >
           <p>
             <>我在創遊社擔任秘書長，負責社團內部的行政事務，並且協助社團的活動舉辦。就任期間，曾代表中央大學赴日參訪鹿兒島大學，</>
-            <>將過程的見聞以 Unity 開發成遊戲 3D 遊戲 </>
+            <>將過程的見聞以 Unity 開發成 3D 遊戲 </>
             <Link href="https://github.com/an920107/kagoshima-monopoly">Kagoshima Monopoly</Link>
             <>，成果被刊登於教學發展中心之</>
             <Link href="https://drive.google.com/file/d/1PSGtWNQGOGbMDn-74UwDhuQbT3wNzfoT/view?usp=sharing">刊物</Link>
@@ -292,12 +292,12 @@ export default function RootPage() {
         </p>
         <p>
           <>大一到大二這段時間中，我透過系上的課程學習到資工相關的基礎知識，並且參加了各項大小團隊，如新生知訊網、</>
-          <>創遊社、服務學習課程負責人，透過這些活動，我不僅加強了個人的程式能力，也學習到了如何與人合作。在這過程中，</>
+          <>創遊社、服務學習課程負責人。透過這些活動，我不僅加強了個人的程式能力，也學習到了如何與人合作。在這過程中，</>
           <>不論課內外專案，都接觸到了各種不同的語言與技術，而我發現，儘管使用的技術在變，其核心價值——<b>軟體工程</b></>
-          <>——確是不變的。若要將技術落地，勢必無法離開軟體工程的範疇，也因此我開始對軟體工程有著濃厚的興趣。</>
+          <>——卻是不變的。若要將技術落地，勢必無法離開軟體工程的範疇，也因此我開始對軟體工程有著濃厚的興趣。</>
         </p>
         <p>
-          <>從大三開始，我從參與專案的身份，轉變為<b>主導專案</b>的角色，開始嘗試透過專案管理方法帶領團隊，提升專案的效率。</>
+          <>從大三開始，我從參與專案的身份，轉變為<b>主導專案</b>的角色，開始嘗試透過<b>專案管理方法</b>帶領團隊，提升專案的效率。</>
           <>我從服務學習課程負責人開始，創立 CSTP 團隊，開發系上的開放式課程平台。另外也<b>與學校、政府合作</b>，開發校務系統</>
           <>以及政府內部系統。而比起書本上的理論，我更好奇業界的軟體工程實務，因此我在大三暑假開始在 LINE Android Team，</>
           <>進行為期一年的實習，經歷到<b>完整的 Scrum 流程</b>，並對於單元測試、設計模式的實踐有了更深的了解。</>
@@ -307,7 +307,7 @@ export default function RootPage() {
         </p>
         <p>
           <>經過這段時間的學習、各式各樣的專案，我希望能夠將自己所學的知識與經驗，付諸實踐，創造出更多有價值的產品。</>
-          <>我以 CIO 的身份與幾位朋友成立艾里空股份有限公司，跟校內 AI 專業的實驗室合作，開發非接觸式智慧偵測裝置。</>
+          <>我以 CIO 的身份與幾位朋友成立艾里空股份有限公司，和校內 AI 專業的實驗室合作，開發非接觸式智慧偵測裝置。</>
           <>我們在過程中有多項獲獎紀錄，亦爭取到許多政府計劃，期望以產學合作的形式，將實驗室的研究成果實際落地。</>
         </p>
         <p>
@@ -327,7 +327,7 @@ export default function RootPage() {
         <h3>讀書計畫與研究計畫</h3>
         <p>
           <>如今我已累積相當的實務經驗，而在理論方面，我希望能有更系統化、更深入的探索，包括但不限於系統設計、物件導向、</>
-          <>設計模式、單元測試等軟領域。而軟體工程為中央資工所之四大研究領域之一，我期待未來能有充分的課程與資源精進自我，</>
+          <>設計模式、單元測試等領域。而軟體工程為中央資工所之四大研究領域之一，我期待未來能有充分的課程與資源精進自我，</>
           <>並針對以下主題進行研究：</>
         </p>
         <p>
